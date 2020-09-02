9fe246073d59dd8ee8eeea404401801d7b2fa814
Ashley Berry <ashleywberry@gmail.com>
2020-09-02 13:37:17 -0500

    added get /activities endpoint

--- server/server.js
+++ server/server.js
@@ -10,6 +10,34 @@ const app = express ();
 // serve static files from server/public folder
 app.use ( express.static ( 'server/public' ) );
 
+// Define a list of activities for my kid
+let activities = [
+    {
+        activity: 'Typing practice',
+        isScreenTime: true,
+        type: 'Mind Exercises'
+    },
+    {
+        activity: 'Bike around the lake',
+        isScreenTime: false,
+        type: 'Body break'
+    },
+    {
+        activity: 'FB Chat with a friend',
+        isScreenTime: true,
+        type: 'Social'
+    },
+];
+
+// Endpoint:
+// GET /activities 
+// Respond with a list of activities
+// ways that we handle the request and response
+app.get ( '/activities', function ( req, res ) {
+    // send back the array of activities
+    res.send ( activities );
+} );
+
 //listen for requests
 //takes two arguments - a port number and an onReady callback function
 const port = 3000;
